ea7fada760b1a5449c19957e81c02d0ae03dfcba
Debmalya <debmalyamajumder91@gmail.com>
2024-03-02 14:24:48 +0530

    added actuator dependency

--- src/main/java/com/mathtrix/MathtrixApplication.java
+++ src/main/java/com/mathtrix/MathtrixApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -21,7 +22,7 @@ import javax.sql.DataSource;
 
 
 @SpringBootApplication
-public class MathtrixApplication {
+public class MathtrixApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
